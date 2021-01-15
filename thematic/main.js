3b0f838fea076cc8556422d1861ece187325bb70
YJWuuu <61780029+YJWuuu@users.noreply.github.com>
2021-01-15 22:55:14 +0800

    Update main.js

--- thematic/main.js
+++ thematic/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-	$('section').on('mouseleave', function() {
+	$('section').on('mouseleave', function() { // mouse leaving a section, show the navi
 		$('nav').css('opacity', '0.7');
 	});
-	$('html').on('click', function() {
+	$('html').on('click', function() { // click on anywhere, hide the navi
 		$('nav').css('opacity', '0');
 	});
 });
